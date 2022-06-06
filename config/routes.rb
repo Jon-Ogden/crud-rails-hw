830e9970eac80c363b241afa7df9eecf582390f4
Jon_Ogden <jogden1863@gmail.com>
2022-06-06 18:14:49 -0500

    everything works

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
+    post '/users', to: 'users#create'
+    put '/users/:id', to: 'users#update'
+    delete '/users/:id', to: 'users#destroy'
+
   end
 
 end
