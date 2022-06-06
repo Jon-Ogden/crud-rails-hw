79c1dd5ac856080597d5155a4d12857b7475cb65
Jon_Ogden <jogden1863@gmail.com>
2022-06-06 15:41:13 -0500

    some small additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username:'Bobylob', age:21)
+User.create(username:'OhioThunder007', age:19)
+User.create(username:'MagicTeacher', age:30)
